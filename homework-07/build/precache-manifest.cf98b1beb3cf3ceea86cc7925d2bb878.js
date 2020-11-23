94ea1b10d1f262496648306a5b18203b4f62801f
RomanPidlisnychyi <myLuckyhelper@gmail.com>
2020-11-23 16:41:29 +0200

    try-CRUD

---
+++ homework-07/build/precache-manifest.cf98b1beb3cf3ceea86cc7925d2bb878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3b72cdcbe43aeab75a1a4a8780acef",
+    "url": "/goit-react-homework/homework-07/build/index.html"
+  },
+  {
+    "revision": "09b233fd1a280483e181",
+    "url": "/goit-react-homework/homework-07/build/static/css/main.b50a206b.chunk.css"
+  },
+  {
+    "revision": "1e1a82d187fe716f2864",
+    "url": "/goit-react-homework/homework-07/build/static/js/2.efa06012.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-homework/homework-07/build/static/js/2.efa06012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b233fd1a280483e181",
+    "url": "/goit-react-homework/homework-07/build/static/js/main.8c03832a.chunk.js"
+  },
+  {
+    "revision": "4ecf7fef25679213ca6e",
+    "url": "/goit-react-homework/homework-07/build/static/js/runtime-main.0253e7d5.js"
+  }
+]);

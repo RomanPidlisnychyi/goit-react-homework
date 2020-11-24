19169d41e8a4465687511bb03f0d22d7709ec2e8
RomanPidlisnychyi <myLuckyhelper@gmail.com>
2020-11-24 20:04:39 +0200

    delete folder

--- homework-07/src/components/Layout/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.div`
-    width: 400px;
-    position: relative;
-`;
--- homework-07/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import phoneBookReduser from './phoneBook/phoneBookReducer';
-
-const store = configureStore({
-    reducer: phoneBookReduser,
-});
-
-export default store;
